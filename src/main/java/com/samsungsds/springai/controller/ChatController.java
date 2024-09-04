cd8a04312c241ad05207732b8d713c9b48c1ad12
yjune-kang <yjune.kang@gmail.com>
2024-09-04 14:34:25 +0900

    단순통신, PromptTemplate 간단구축 완료

--- src/main/java/com/samsungsds/springai/controller/ChatController.java
+++ src/main/java/com/samsungsds/springai/controller/ChatController.java
@@ -10,6 +10,7 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,6 +29,7 @@ public class ChatController {
     }
 
     @PostMapping("/capitalWithInfo")
+    @ResponseBody
     @Operation(summary = "포멧이 있는 응답 출력", description = "아래의 프롬프트로 응답 형식을 지정 해 준 수도 관련 질문에 대한 응답을 출력 한다. : <br> " +
             "{country} 나라의 수도는 어디인지와 아래 형태로 대답해줘: <br>" +
             "           \\```country\\``` 나라의 수도는 \\```capital\\```.<br>" +
@@ -39,13 +41,15 @@ public class ChatController {
     }
 
     @PostMapping("/capital")
-    @Operation(summary = "", description = "아래의 프롬프트를 실행한다. : <br>" +
-            " {country} 나라의 수도는 어디?<br> ")
+    @ResponseBody
+    @Operation(summary = "수도 프롬프트 실행", description = "아래의 프롬프트를 실행한다. : <br>" +
+            " {country} 나라의 수도는 어디인가?<br> ")
     public CapitalResponse getCapital(@Parameter(description = "수도를 알고싶은 국가나 주 이름 -> {country}", example = "대한민국") @RequestBody CapitalRequest capitalRequest) {
         return this.chatService.getCapital(capitalRequest);
     }
 
     @PostMapping("/ask")
+    @ResponseBody
     @Operation(summary = "자유 프롬프트 실행", description = "별도 포멧 없이 사용자의 입력을 그대로 실행")
     public Answer askQuestion(@Parameter(description = "실행 할 프롬프트") @RequestBody Question question) {
         return chatService.getAnswer(question);
